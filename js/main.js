9f26cbbf0dcc51263652a7c173c923765c605d6f
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2017-05-07 19:17:12 +0800

    finished testimonials section

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+  $(".start").mouseenter(function(){
+    if($(".startu").hasClass("off")){
+      $(".startu").animate({
+        width: "+=125"
+      }, 500);
+      $(".startu").removeClass("off").addClass("on");
+    }
+  })
+  $(".start").mouseout(function(){
+    if($(".startu").hasClass("on")){
+      $(".startu").animate({
+        width: "-=125"
+      }, 500);
+      $(".startu").removeClass("on").addClass("off");
+    }
+  })
+
+  //slick.js
+  $("#testimonials").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    nextArrow: "<i class='fa fa-arrow-circle-right next-arrow'></i>",
+    prevArrow: "<i class='fa fa-arrow-circle-left prev-arrow'></i>"
+  });
+
+  //load recommendations linkedin api
+
+})
