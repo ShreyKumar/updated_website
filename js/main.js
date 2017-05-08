a40e5e0abbacebf8ff7c0f2e7d70ede9fa6a819b
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2017-05-09 02:03:11 +0800

    finished adding mac screen and slick for projects

--- js/main.js
+++ js/main.js
@@ -24,7 +24,14 @@ $(function(){
     nextArrow: "<i class='fa fa-arrow-circle-right next-arrow'></i>",
     prevArrow: "<i class='fa fa-arrow-circle-left prev-arrow'></i>"
   });
-
+  
+  $("#projects-container").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    nextArrow: "<i class='fa fa-arrow-circle-right next-arrow'></i>",
+    prevArrow: "<i class='fa fa-arrow-circle-left prev-arrow'></i>"
+  });
   //load recommendations linkedin api
 
 })
