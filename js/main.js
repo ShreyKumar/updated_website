53dce5af64dc5337c7c6c69d8ef4f83cd209872c
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2017-05-09 18:54:09 +0800

    fixed outline width issue, finished basic front end for projects

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(function(){
     nextArrow: "<i class='fa fa-arrow-circle-right next-arrow'></i>",
     prevArrow: "<i class='fa fa-arrow-circle-left prev-arrow'></i>"
   });
-  
+
   $("#projects-container").slick({
     autoplay: true,
     autoplaySpeed: 5000,
@@ -32,6 +32,8 @@ $(function(){
     nextArrow: "<i class='fa fa-arrow-circle-right next-arrow'></i>",
     prevArrow: "<i class='fa fa-arrow-circle-left prev-arrow'></i>"
   });
+
+
   //load recommendations linkedin api
 
 })
