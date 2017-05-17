80d909fb859e235443ee3aa1ce4bc8697d75ddec
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2017-05-17 14:35:14 -0400

    this should fix navbar bug

--- js/responsive.js
+++ js/responsive.js
@@ -3,6 +3,17 @@ $(function(){
   $(".bars").click(function(){
     $("nav").slideToggle();
   })
+  //close nav if click on a link or if scrolls past element
+  $("nav .lnk a").click(function(){
+    $("nav").slideUp();
+  });
+  var past_length = $("#home").offset().top + $("#home").outerHeight();
+  $(window).scroll(function(){
+    if($(window).scrollTop() > past_length){
+      $("nav").slideUp();
+    }
+  })
+
   $(window).resize(function(){
     if($(window).width() > 320){
       $("nav").show();
